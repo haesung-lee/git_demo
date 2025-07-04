584d8d6f5da1a42d2d783c1c6881aaab7dbf67bd
haesung-lee <haesung7.lee@gmail.com>
2025-07-04 20:26:37 +0900

    feat: modify calulator4

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,15 @@ public class Calculator  {
     public long add(String str1, String str2) {
         int a = Integer.parseInt(str1);
         int b = Integer.parseInt(str2);
+
+        if(a < b){
+            for(int i = 0; i<10; i++){
+                System.out.println("!!!");
+            }
+        }else {
+            System.out.println("!!!");
+        }
+
         long longA = a;
         long longB = b;
         return longA + longB;
